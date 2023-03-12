fdeb997202a2dd4658d8ef298751b30acd2908e8
Mark <markfamiliytitan@gmail.com>
2023-03-12 23:01:36 +0200

    розніс компоненти

--- src/component/Woozley/Logic/Logic.js
+++ src/component/Woozley/Logic/Logic.js
@@ -1,4 +1,106 @@
-const Logic = () => { 
-    return <></>;
-}
-export default Logic
+
+const streaming = {
+  Ultra: "4k/2160p Ultra HD",
+  Full: "1080p Full HD",
+  HD: "480p",
+};
+
+export const tableCloud = {
+  NorthAm: {
+    title: "North America",
+    Latency: 23,
+    Download: 9,
+    Video: streaming.Ultra,
+  },
+  SouAm: {
+    title: "South America",
+    Latency: 185,
+    Download: 101,
+    Video: streaming.Full,
+  },
+  Eur: {
+    title: "Europe",
+    Latency: 11,
+    Download: 5,
+    Video: streaming.Ultra,
+  },
+  Asia: {
+    title: "Asia",
+    Latency: 73,
+    Download: 41,
+    Video: streaming.Ultra,
+  },
+  Ocea: {
+    title: "Okeania",
+    Latency: 93,
+    Download: 63,
+    Video: streaming.Ultra,
+  },
+};
+
+export const Storage_0010 = {
+  NorthAm: {
+    title: "North America",
+    Latency: 100,
+    Download: 70,
+    Video: streaming.Ultra,
+  },
+  SouAm: {
+    title: "South America",
+    Latency: 314,
+    Download: 92,
+    Video: streaming.Full,
+  },
+  Eur: {
+    title: "Europe",
+    Latency: 11,
+    Download: 5,
+    Video: streaming.Ultra,
+  },
+  Asia: {
+    title: "Asia",
+    Latency: 149,
+    Download: 61,
+    Video: streaming.Ultra,
+  },
+  Ocea: {
+    title: "Okeania",
+    Latency: 266,
+    Download: 165,
+    Video: streaming.HD,
+  },
+};
+
+export const Storage_0100 = {
+  NorthAm: {
+    title: "North America",
+    Latency: 23,
+    Download: 9,
+    Video: streaming.Ultra,
+  },
+//   SouAm: {
+//     title: "South America",
+//     Latency: 314,
+//     Download: 92,
+//     Video: streaming.Full,
+//   },
+  Eur: {
+    title: "Europe",
+    Latency: 85,
+    Download: 62,
+    Video: streaming.Ultra,
+  },
+//   Asia: {
+//     title: "Asia",
+//     Latency: 149,
+//     Download: 61,
+//     Video: streaming.Ultra,
+//   },
+//   Ocea: {
+//     title: "Okeania",
+//     Latency: 266,
+//     Download: 165,
+//     Video: streaming.HD,
+//   },
+};
+
---
+++ src/component/Woozley/Ui/CommLines/Patern/Patern.js
@@ -0,0 +1,52 @@
+import { ImgPico } from "../../Ui.styled";
+import { DivBoxImg, DivComm } from "../CommLines.stiled";
+import CONSTMAP from "./CONSTMAP";
+
+
+ const RenderPriv = (Line, dev) => {
+   return Line.map((i, index) => {
+     const devIndex = Math.floor(index / 3);
+     const devCount = dev[devIndex];
+     const imgIndex = index % 3;
+     const shouldRenderImg = devCount >= imgIndex;
+
+     return (
+       <DivComm key={i}>
+         {shouldRenderImg ? <ImgPico src={i} alt={`link_${index}`} /> : null}
+       </DivComm>
+     );
+   });
+ };
+
+  const createResultCentral = (CEN) => {
+    let resultCentral = "";
+
+    for (let i = 0; i < 4; i++) {
+      if (i === CEN) {
+        resultCentral += "1";
+      } else {
+        resultCentral += "0";
+      }
+    }
+
+    return resultCentral;
+  };
+
+
+const RenderLine = (DEV, SER, CEN, looking) => {
+  const res = SER.map(String).reduce((a, b) => a + b);
+  const result = looking ? `SE_${res}` : `CSE_${createResultCentral(CEN)}`;
+
+  const MAPSERVER = CONSTMAP(DEV);
+
+  const elem = [];
+  for (let i = 0; i < MAPSERVER[result].length; i++) {
+    const firstElem = MAPSERVER[result][i][0];
+    const rest = MAPSERVER[result][i].slice(1);
+    elem.push(RenderPriv(firstElem, rest));
+  }
+
+  return <DivBoxImg key={elem}>{elem}</DivBoxImg>;
+};
+
+export default RenderLine;
